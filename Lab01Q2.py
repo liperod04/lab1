c3127247382e3c1b911b3640c0b65875d91523a7
Filipe Rodrigues <162600548+liperod04@users.noreply.github.com>
2024-03-07 10:34:15 -0500

    Delete Lab01Q2.py

--- Lab01Q2.py
+++
@@ -1,8 +0,0 @@
-user_input = str(input('Enter integer: ')) # Takes in user input integer
-
-digit_count = len(user_input) # Takes length/number of digits from user input
-
-if digit_count == 1: # Checks if "digit" should be set to singular or plural
-    print(f'{user_input} has {digit_count} digit')
-else:
-    print(f'{user_input} has {digit_count} digits')
